42931e0979617942111b91c2ebbb90f62aa24a59
vdc109 <93813570+vdc109@users.noreply.github.com>
2022-09-27 01:00:01 +0700

    Update index.vue

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -6,101 +6,116 @@
       h1.header VINH DO
       v-spacer
     .content
-      h3 EDUCATION
+      h3 CONTACT
+      hr.sep-bar
+      span Email: vinhdc109@gmail.com
+      v-spacer
+      span Phone: (+84)855595596
+      h3.head2 EDUCATION
       hr.sep-bar
       span Hanoi Amsterdam Highschool for the gifted - Specialization in Mathematics
       span.duration 2020-2023
       v-spacer
-      span
+      a(href="https://drive.google.com/drive/folders/1PkPbNcXz3fA2vantSPIkC0kD4KGuiGfe?usp=sharing" target="_blank")
         b GPA:
       .GPA
         span Grade 9:
         span Grade 10: 8.9
         span Grade 11: 9.4
       v-spacer
-      span 
+      a(href="https://drive.google.com/drive/folders/15h4-zQHYPwjYumk2LKY9i530U1EQ1R6B?usp=sharing" target="_blank")
         b Standardized test scores:
       .standardized
         span IELTS: 8.0
         span SAT: 1520 (Mathematics: 800 | Critical Reading & Writing: 720)
       v-spacer
-      span
+      a(href="https://drive.google.com/drive/folders/175QrTSLQ0IfQVMFAnXItQvPX8pZ_IC4f?usp=sharing" target="_blank")
         b Advanced placement:
       .ap
-        span AP Calculus BC: 4 (Subscore Calculus AB: 5)
-        span AP Computer Science A: 4
+        span AP Calculus BC: 5 (Subscore Calculus AB: 5)
+        span AP Computer Science A: 5
         span AP Statistics: 4
 
-      h3.head2 HONOURS AND MERITS
+      h3.head2 HONORS AND MERITS
       hr.sep-bar
       span
         b Domestic & Regional:
       ul
         li
-          span Mathematical Young Talent Search 2017 - Gold Medal
+          a(href="https://drive.google.com/file/d/1FnDgdj_Dwc4T2QWMXvZAQt24hUGP1Ios/view?usp=sharing" target="_blank") Mathematical Young Talent Search 2017 - Gold Medal
         li
-          span Mathematical Young Talent Search 2018 - Silver Medal
+          a(href="https://drive.google.com/file/d/1auBfTmACiccIXq3zCtqarj1xTRpcds2R/view?usp=sharing" target="_blank") Mathematical Young Talent Search 2018 - Silver Medal
         li
-          span Hanoi Mathematics Olympiad 2019 - 2nd Prize
+          a(href="https://drive.google.com/file/d/1K7NfLC7oEW5eh49WEjVh7ggT6U9d0BEl/view?usp=sharing" target="_blank") Hanoi Mathematics Olympiad 2019 - 2nd Prize
         li
-          span Hanoi Informatics Olympiad 2020 - 3rd Prize
-        li
-          span Hanoi Informatics Olympiad 2021 - 3rd Prize
+          a(href="https://drive.google.com/file/d/1PjEmKkdjq2q7dL8yJEBnEuaJHahGNAkl/view?usp=sharing" target="_blank") Hanoi Informatics Olympiad 2020 - 3rd Prize
 
       .international
         span
           b International:
         ul
           li
-            span 14th International Mathematics and Science Olympiad 2017 (IMSO) - Silver Medal
+            a(href="https://drive.google.com/file/d/1yBxImI6cHkEYxj57luzHEqEOrrzwXbVM/view?usp=sharing" target="_blank") 14th International Mathematics and Science Olympiad 2017 (IMSO) - Silver Medal
           li
-            span Asian - Pacific Mathematical Olympiad for Primary Schools 2017 (APMOPS) - Silver Medal
+            a(href="https://drive.google.com/file/d/1qUZGa7Zto-8fFAUr3sJ7kT2etvK8QRXN/view?usp=sharing" target="_blank") Asian - Pacific Mathematical Olympiad for Primary Schools 2017 (APMOPS) - Silver Medal
           li
-            span 7th International Mathematical Modelling Challenge 2021 (IMMC)
+            a(href="https://drive.google.com/drive/folders/1MS6S9aA3Owu_HdRd2PyWd8EmJxCkvnSX?usp=sharing" target="_blank") 7th International Mathematical Modelling Challenge 2021 (IMMC)
 
       h3.head3 RESEARCH PROJECTS
       hr.sep-bar
       ul
         li 
-          span 
+          a(href="https://drive.google.com/drive/folders/1FpwChxGljIMtqD07thaTlqGjjj4Bs-M4?usp=sharing" target="_blank")
             b E-NNOVATE International Innovation Show, Poland - Gold Medal
-            b.time1 June 2022
+          b.time1 June 2022
           v-spacer
-          span Project: "Design of Distributed Academic-record System based on Blockchain"
-        li 
-          span 
+          span Contributed to project: "Design of Distributed Academic-record System based on Blockchain"
+        li
+          a(href="https://drive.google.com/drive/folders/1UuoFw0nAvxm-CDYMU7cwCpVYc3ZSqyEQ?usp=sharing" target="_blank")
+            b Global Link Online Innovation Expo Asia 2022 - 1st Place
+          b.time1 August 2022
+          v-spacer
+          span Contributed and presented project: "Fabrication of Vanadium-doped TiO2 for Polluted Wastewater Treatment"
+        li
+          a(href="https://drive.google.com/drive/folders/1EH29X2PUFuqMjcTEG3_uglgZLeQQ8UIn?usp=sharing" target="_blank")
             b INNOVERSE Innovation & Invention Expo, USA - Gold Medal
-            b.time1 July 2022
+          b.time1 July 2022
           v-spacer
-          span Project: "Fabrication of Vanadium-doped TiO2 for Polluted Wastewater Treatment"
+          span Contributed to project: "Making Use of Speed Braker to Generate Electricity"
 
       h3.head4 EXTRACURRICULARS AND SOCIAL ACTIVITIES
       hr.sep-bar
       ul
         li
-          span 
+          a(href="https://drive.google.com/drive/folders/1a7220MDCPqHej8CFgYG1JxOTSmsgcc-O?usp=sharing" target="_blank")
             b Project Compyter
-            b.time1 June 2021
+          b.time1 June 2021
           v-spacer
           span Teaching online Python course for 45 secondary school students and raise $800 for charity
         li
-          span 
-            b Charity for Children's Education
-            b.time1 July 2021
+          a(href="https://drive.google.com/drive/folders/1hWmt59-pAiEaP9WvgZRrqbFo3GNhCD_N?usp=sharing" target="_blank")
+            b Charity for Children's Education - Ha Giang
+          b.time1 July 2021
           v-spacer
           span Raised $3100, 10 computers, books, bags and clothes from companies and benefactors to donate to poor students in Ha Giang
         li
           span 
-            b Helping the blinds
+            b Helping the blinds in Nam Dinh
             b.time1 August 2021
           v-spacer
-          span Create supporting devices by 3D printing and Arduino coding for blind people at Hanoi Center for the Blind
+          span Create supporting devices by 3D printing and Arduino coding for blind people at Nam Dinh Center for the Blind
         li
-          span 
-            b Raising technology awareness for gifted students in Tuyen Quang
-            b.time1 May 2021
+          a(href="https://drive.google.com/drive/folders/11dCBIHj9sc6qXkWzSVgAdscMKKGmBjeA?usp=sharing" target="_blank")
+            b Organising events for children in Lao Cai
+          b.time1 March 2021
           v-spacer
-          span Teaching coding basis in C++, Arduino coding for gift students in Tuyen Quang to create air filtering and monitoring devices
+          span Creating activities and reward students in Lao Cai who excelled in a math competition
+        li
+          a(href="https://drive.google.com/drive/folders/1wQZv4ttIhoR1AcL5BdAscElR-NmunkEn?usp=sharing" target="_blank")
+            b Raising technology awareness for gifted students in Tuyen Quang
+          b.time1 May 2021
+          v-spacer
+          span Teaching coding basics in C++, Arduino coding for gift students in Tuyen Quang to create air filtering and monitoring devices
         li
           span 
             b Teaching assistant in Math classes for a center
@@ -108,36 +123,50 @@
           v-spacer
           span Assist in teaching, marking homework and communicating with parents of children in online Math classes of an IMO Silver Medalist
         li
-          span
-            b Content editor for Facebook group Tran Le English
-            b.time1 October 2019 - July 2021
+          a(href="https://drive.google.com/drive/folders/13jr3A9m_Sywcjv6WuxY09aLppmy3wbuB?usp=sharing" target="_blank")
+            b Content writer for STEM and English Facebook groups
+          b.time1 October 2019 - July 2021
           v-spacer
           span Content editor for Facebook group about English books
+        li
+          a(href="https://drive.google.com/file/d/1KHf9LxMEqrZvHJOPmWOQONsO7MzvS4rr/view?usp=sharing" target="_blank")
+            b Completed Harvard's CS50 2022
+          b.time1 February 2022
+          v-spacer
+          span Completed all lessons, labs, problem sets and a final project of CS50
         li
           span
             b Making e-commerce website for Tran Le Bookstore
             b.time1 June 2022 - July 2022
           v-spacer
-          span Develop frontend and backend for a English books shopping website
+          span Develop frontend and backend for an English books shopping website
 
       h3.head2 LEADERSHIP
       hr.sep-bar
+      ul
+        li
+          span
+            b Co-founder and leader of Edutech club
+            b.time1
+          v-spacer Organize events and activities to help children in less developed provinces (Tuyen Quang, Lao Cai, Ha Giang, ...)
 
       h3.head2 INTERNSHIP AND WORKING EXPERIENCE
       hr.sep-bar
       ul
         li
-          span
+          a(href="https://drive.google.com/drive/folders/1EpOV9QVIRmiI9ZYVI03B5h3capzUx5wZ?usp=sharing" target="_blank")
             b Hanoi University of Science and Technology Lab
-            b.time1 June 2021 - July 2021
+          b.time1 June 2021 - July 2021
           v-spacer
           span Making call bots by creating workflow and connecting to AI call center for companies and industries
         li
-          span
-            b International MedTech software company - ISOFH
-            b.time1 July 2021 - September 2021
+          a(href="https://drive.google.com/drive/folders/1EqFUZ_aNQj5VX8-mc1d6OJVUzne59V6L?usp=sharing" target="_blank")
+            b International MedTech software company ISOFH
+          b.time1 July 2021 - September 2021
           v-spacer
           span Fixing bugs and developing the web hosting the company's Blockchain project IVIRSE
+
+      .footer
 </template>
 
 <script lang="ts">
@@ -162,12 +191,13 @@ export default Home;
   justify-content: center
 
 .content
-  margin: 15px 70px 5px 70px
+  margin: 15px auto
+  max-width: 85%
   font-family: Georgia, Serif
 
 .paper
   height: 100%
-  margin: 0 auto
+  margin: 5px auto
   max-width: 65%
   background-color: #fff
 
