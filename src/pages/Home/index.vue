a1d0f49459b8da6f6cfa6a75a25d3c713b251ea3
vdc109 <93813570+vdc109@users.noreply.github.com>
2022-10-12 20:46:54 +0700

    Update index.vue

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -13,14 +13,14 @@
       span Phone: (+84)855595596
       h3.head2 EDUCATION
       hr.sep-bar
-      span Hanoi Amsterdam Highschool for the gifted - Specialization in Mathematics
+      span Hanoi Amsterdam Highschool for the gifted - Mathematics Honor Class
       span.duration 2020-2023
       v-spacer
       a(href="https://drive.google.com/drive/folders/1PkPbNcXz3fA2vantSPIkC0kD4KGuiGfe?usp=sharing" target="_blank")
         b GPA:
       .GPA
-        span Grade 9:
-        span Grade 10: 8.9
+        span Grade 9: 8.8
+        span Grade 10: 8.8
         span Grade 11: 9.4
       v-spacer
       a(href="https://drive.google.com/drive/folders/15h4-zQHYPwjYumk2LKY9i530U1EQ1R6B?usp=sharing" target="_blank")
@@ -35,6 +35,13 @@
         span AP Calculus BC: 5 (Subscore Calculus AB: 5)
         span AP Computer Science A: 5
         span AP Statistics: 4
+      v-spacer
+      span
+        b Extracurricular coursework:
+        ul
+          li
+            a(href="https://drive.google.com/file/d/1KHf9LxMEqrZvHJOPmWOQONsO7MzvS4rr/view?usp=sharing" target="_blank") Completed all of Harvard's CS50 2022 problemsets, labs, and final project
+
 
       h3.head2 HONORS AND MERITS
       hr.sep-bar
@@ -61,27 +68,48 @@
           li
             a(href="https://drive.google.com/drive/folders/1MS6S9aA3Owu_HdRd2PyWd8EmJxCkvnSX?usp=sharing" target="_blank") 7th International Mathematical Modelling Challenge 2021 (IMMC)
 
+      .international
+        span
+          b Other honors and merits:
+        ul
+          li
+            span Certificate of Contribution for Community by Ho Chi Minh Youth Executive Committee (2022)
+          li
+            span AP Scholar 2022
+
       h3.head3 RESEARCH PROJECTS
       hr.sep-bar
       ul
         li 
           a(href="https://drive.google.com/drive/folders/1FpwChxGljIMtqD07thaTlqGjjj4Bs-M4?usp=sharing" target="_blank")
-            b E-NNOVATE International Innovation Show, Poland - Gold Medal
+            b E-NNOVATE International Innovation Show 2022, Poland - Gold Medal
           b.time1 June 2022
           v-spacer
-          span Contributed to project: "Design of Distributed Academic-record System based on Blockchain"
+          span Project: "Design of Distributed Academic-record System based on Blockchain"
         li
           a(href="https://drive.google.com/drive/folders/1UuoFw0nAvxm-CDYMU7cwCpVYc3ZSqyEQ?usp=sharing" target="_blank")
-            b Global Link Online Innovation Expo Asia 2022 - 1st Place
+            b Global Link Online Innovation Expo 2022, Japan - 1st Place
           b.time1 August 2022
           v-spacer
-          span Contributed and presented project: "Fabrication of Vanadium-doped TiO2 for Polluted Wastewater Treatment"
+          span Project: "Fabrication of Vanadium-doped TiO2 for Polluted Wastewater Treatment"
         li
           a(href="https://drive.google.com/drive/folders/1EH29X2PUFuqMjcTEG3_uglgZLeQQ8UIn?usp=sharing" target="_blank")
-            b INNOVERSE Innovation & Invention Expo, USA - Gold Medal
+            b INNOVERSE Innovation & Invention Expo 2022, USA - Gold Medal
           b.time1 July 2022
           v-spacer
-          span Contributed to project: "Making Use of Speed Braker to Generate Electricity"
+          span Project: "Making Use of Speed Braker to Generate Electricity"
+      h3.head3 PERSONAL PROJECTS
+      hr.sep-bar
+      ul
+        li
+          span
+            b Assistive stick for the blind
+          b.time1 June 2022
+          v-spacer
+          span Ultrasonic sensor stick warning dangerous obstacles for the blind
+          v-spacer
+          span Github link:
+          a(href="https://github.com/vdc109/Assistive-stick-for-the-blind" target="_blank") https://github.com/vdc109/Assistive-stick-for-the-blind
 
       h3.head4 EXTRACURRICULARS AND SOCIAL ACTIVITIES
       hr.sep-bar
@@ -89,27 +117,27 @@
         li
           a(href="https://drive.google.com/drive/folders/1a7220MDCPqHej8CFgYG1JxOTSmsgcc-O?usp=sharing" target="_blank")
             b Project Compyter
-          b.time1 June 2021
+          b.time1 May 2022
           v-spacer
-          span Teaching online Python course for 45 secondary school students and raise $800 for charity
+          span Teaching online Python course for 41 secondary school students and raise 17 million VND for charity
         li
           a(href="https://drive.google.com/drive/folders/1hWmt59-pAiEaP9WvgZRrqbFo3GNhCD_N?usp=sharing" target="_blank")
             b Charity for Children's Education - Ha Giang
-          b.time1 July 2021
+          b.time1 June 2022
           v-spacer
-          span Raised $3100, 10 computers, books, bags and clothes from companies and benefactors to donate to poor students in Ha Giang
+          span Raised 100 million VND, 11 computers, books, bags and clothes from companies and benefactors to donate to poor students in Ha Giang
         li
-          span 
+          a(href="https://drive.google.com/drive/folders/1EoKiGYYm77atDrk78ZFO23vg_2cbWdYc?usp=sharing" target="_blank")
             b Helping the blinds in Nam Dinh
-            b.time1 August 2021
+            b.time1 September 2021
           v-spacer
-          span Create supporting devices by 3D printing and Arduino coding for blind people at Nam Dinh Center for the Blind
+          span Create supporting devices by 3D printing and Arduino coding for blind people at Nam Dinh Center for the Blind and donate 20 million VND to the people and education funds of blind people
         li
           a(href="https://drive.google.com/drive/folders/11dCBIHj9sc6qXkWzSVgAdscMKKGmBjeA?usp=sharing" target="_blank")
             b Organising events for children in Lao Cai
           b.time1 March 2021
           v-spacer
-          span Creating activities and reward students in Lao Cai who excelled in a math competition
+          span Create activities and reward students in Lao Cai who excelled in a math competition
         li
           a(href="https://drive.google.com/drive/folders/1wQZv4ttIhoR1AcL5BdAscElR-NmunkEn?usp=sharing" target="_blank")
             b Raising technology awareness for gifted students in Tuyen Quang
@@ -123,23 +151,32 @@
           v-spacer
           span Assist in teaching, marking homework and communicating with parents of children in online Math classes of an IMO Silver Medalist
         li
-          a(href="https://drive.google.com/drive/folders/13jr3A9m_Sywcjv6WuxY09aLppmy3wbuB?usp=sharing" target="_blank")
-            b Content writer for STEM and English Facebook groups
-          b.time1 October 2019 - July 2021
-          v-spacer
-          span Content editor for Facebook group about English books
-        li
-          a(href="https://drive.google.com/file/d/1KHf9LxMEqrZvHJOPmWOQONsO7MzvS4rr/view?usp=sharing" target="_blank")
-            b Completed Harvard's CS50 2022
-          b.time1 February 2022
-          v-spacer
-          span Completed all lessons, labs, problem sets and a final project of CS50
-        li
           span
             b Making e-commerce website for Tran Le Bookstore
             b.time1 June 2022 - July 2022
           v-spacer
           span Develop frontend and backend for an English books shopping website
+          v-spacer
+          span Website link:
+          a(href="https://tranlebookstore.com/" target="_blank") https://tranlebookstore.com/
+        li
+          a(href="https://drive.google.com/drive/folders/1MrNQ4lD80jJbxGmMm2NPX8LUxPN93N8M?usp=sharing" target="_blank")
+            b Wrote answer keys for A Closer Look - Ohio grade 1-6
+          b.time1 December 2017 - February 2018
+          v-spacer
+          span Wrote an answer key book to help financially students who wish to self-study science (from $200 publisher's price to $6)
+        li
+          span
+            b Member of GART - Hanoi Amsterdam Robotic Club
+            b.time1 September 2020 - Now
+          v-spacer
+          span Help teach young students technology and organize STEM related events at schools or the US embassy
+        li
+          span
+            b Volunteer in STEM summer camp
+            b.time1 August 2018
+          v-spacer
+          span Help organize STEM summer camp in science experiments for 150 primary school students in Hanoi
 
       h3.head2 LEADERSHIP
       hr.sep-bar
@@ -147,8 +184,26 @@
         li
           span
             b Co-founder and leader of Edutech club
-            b.time1
-          v-spacer Organize events and activities to help children in less developed provinces (Tuyen Quang, Lao Cai, Ha Giang, ...)
+            b.time1 November 2021 - Now
+          v-spacer Organize events and activities to help unfortunate people in less developed provinces
+        li
+          span
+            b Admin group Facebook SuTi cùng con hoc Toán
+          b.time1 June 2021 - Now
+          v-spacer
+          span Sharing online resources, learning tips and methods in STEM and Facebook groups of parents of younger students
+          v-spacer
+          span Group link:
+          a(href="https://www.facebook.com/groups/3765086493543619" target="_blank") https://www.facebook.com/groups/3765086493543619
+        li
+          span
+            b Admin group English Book Corner
+          b.time1 April 2021 - Now
+          v-spacer
+          span Sharing online resources, learning tips and review and recommending books for younger students
+          v-spacer
+          span Group link:
+          a(href="https://www.facebook.com/groups/169378198219139" target="_blank") https://www.facebook.com/groups/169378198219139
 
       h3.head2 INTERNSHIP AND WORKING EXPERIENCE
       hr.sep-bar
@@ -156,15 +211,22 @@
         li
           a(href="https://drive.google.com/drive/folders/1EpOV9QVIRmiI9ZYVI03B5h3capzUx5wZ?usp=sharing" target="_blank")
             b Hanoi University of Science and Technology Lab
-          b.time1 June 2021 - July 2021
+          b.time1 April 2022 - June 2022
           v-spacer
           span Making call bots by creating workflow and connecting to AI call center for companies and industries
         li
           a(href="https://drive.google.com/drive/folders/1EqFUZ_aNQj5VX8-mc1d6OJVUzne59V6L?usp=sharing" target="_blank")
             b International MedTech software company ISOFH
-          b.time1 July 2021 - September 2021
+          b.time1 July 2022 - August 2022
           v-spacer
-          span Fixing bugs and developing the web hosting the company's Blockchain project IVIRSE
+          span Fixing bugs and developing the web frontend hosting the company's Blockchain project IVIRSE
+        li
+          span
+            b Teaching Assistant SuTi Maths
+          b.time1 March 2021 - February 2022
+          v-spacer
+          span Assisted teachers in preparing teaching materials for 2 classes of 25 students and marked students' homework and tests
+
 
       .footer
 </template>
