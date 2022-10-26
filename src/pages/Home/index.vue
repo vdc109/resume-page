df23fdc1146d9f2fad27f18d00acca8a828b3e3e
vdc109 <93813570+vdc109@users.noreply.github.com>
2022-10-27 00:41:29 +0700

    Update index.vue

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -6,25 +6,14 @@
       h1.header VINH DO
       v-spacer
     .content
-      h3 CONTACT
-      hr.sep-bar
-      span Email: vinhdc109@gmail.com
-      v-spacer
-      span Phone: (+84)855595596
-      v-spacer
-      span Facebook: 
-        a(href="https://www.facebook.com/vinhdc109" target="_blank") https://www.facebook.com/vinhdc109
       h3.head2 EDUCATION
       hr.sep-bar
       span Hanoi Amsterdam Highschool for the gifted - Mathematics Honor Class
       span.duration 2020-2023
       v-spacer
-      a(href="https://drive.google.com/drive/folders/1PkPbNcXz3fA2vantSPIkC0kD4KGuiGfe?usp=sharing" target="_blank")
-        b GPA:
-      .GPA
-        span Grade 9: 8.8
-        span Grade 10: 8.7
-        span Grade 11: 9.4
+      span 
+        b GPA: 
+        span 9.4
       v-spacer
       a(href="https://drive.google.com/drive/folders/15h4-zQHYPwjYumk2LKY9i530U1EQ1R6B?usp=sharing" target="_blank")
         b Standardized test scores:
@@ -40,8 +29,8 @@
           li
             a(href="https://drive.google.com/drive/folders/175QrTSLQ0IfQVMFAnXItQvPX8pZ_IC4f?usp=sharing" target="_blank") Advanced placement:
             .ap
-              span AP Calculus BC: 5 (Subscore Calculus AB: 5)
-              span AP Computer Science A: 5
+              span AP Calculus BC: 4 (Subscore Calculus AB: 5)
+              span AP Computer Science A: 4
               span AP Statistics: 4
 
       h3.head2 HONORS AND MERITS
@@ -50,35 +39,28 @@
         b Domestic & Regional:
       ul
         li
-          a(href="https://drive.google.com/file/d/1FnDgdj_Dwc4T2QWMXvZAQt24hUGP1Ios/view?usp=sharing" target="_blank") Mathematical Young Talent Search 2017 - Gold Medal
+          span Hanoi Mathematics Municipal Academic Competition 2020 - 2nd Prize
         li
-          a(href="https://drive.google.com/file/d/1auBfTmACiccIXq3zCtqarj1xTRpcds2R/view?usp=sharing" target="_blank") Mathematical Young Talent Search 2018 - Silver Medal
+          span Hanoi Informatics Municipal Academic Competition 2021 - 3rd Prize
         li
-          a(href="https://drive.google.com/file/d/1K7NfLC7oEW5eh49WEjVh7ggT6U9d0BEl/view?usp=sharing" target="_blank") Hanoi Mathematics Olympiad 2019 - 2nd Prize
-        li
-          a(href="https://drive.google.com/file/d/1PjEmKkdjq2q7dL8yJEBnEuaJHahGNAkl/view?usp=sharing" target="_blank") Hanoi Informatics Olympiad 2020 - 3rd Prize
+          span Mathematical Young Talent Search 2017 - Gold Medal (Competition info: 
+          a(href="https://www.hexagon.edu.vn/myts.html" target="_blank") https://www.hexagon.edu.vn/myts.html
+          span )
 
       .international
         span
           b International:
         ul
           li
-            a(href="https://drive.google.com/file/d/1yBxImI6cHkEYxj57luzHEqEOrrzwXbVM/view?usp=sharing" target="_blank") 14th International Mathematics and Science Olympiad 2017 (IMSO) - Silver Medal
+            span 14th International Mathematics and Science Olympiad 2017 (IMSO) - Silver Medal (Competition info: 
+            a(href="https://www.sofuganda.org/imso" target="_blank") https://www.sofuganda.org/imso
+            span )
           li
-            a(href="https://drive.google.com/file/d/1qUZGa7Zto-8fFAUr3sJ7kT2etvK8QRXN/view?usp=sharing" target="_blank") Asian - Pacific Mathematical Olympiad for Primary Schools 2017 (APMOPS) - Silver Medal
-          li
-            a(href="https://drive.google.com/drive/folders/1MS6S9aA3Owu_HdRd2PyWd8EmJxCkvnSX?usp=sharing" target="_blank") 7th International Mathematical Modelling Challenge 2021 (IMMC)
+            span 7th International Mathematical Modelling Challenge 2021 (IMMC) (Competition info: 
+            a(href="https://www.immchallenge.org/Index.html" target="_blank") https://www.immchallenge.org/Index.html
+            span )
 
-      .international
-        span
-          b Other honors and merits:
-        ul
-          li
-            span Certificate of Contribution for Community by Ho Chi Minh Youth Executive Committee (2022)
-          li
-            span AP Scholar 2022
-
-      h3.head3 RESEARCH PROJECTS
+      h3.head3 RESEARCH & PERSONAL PROJECTS
       hr.sep-bar
       ul
         li
@@ -99,28 +81,27 @@
           b.time1 June 2022
           v-spacer
           span Project: "Design of Distributed Academic-record System based on Blockchain"
-
-
-
-      h3.head3 PERSONAL PROJECTS
-      hr.sep-bar
-      ul
         li
           span
-            b Resume site
+            b Resume site   
+            span (Project link: 
+            a(href="https://github.com/vdc109/resume-page" target="_blank") https://github.com/vdc109/resume-page
+            span )
           b.time1 August 2022 - October 2022
           v-spacer
           span Resume website based with descriptive links
           v-spacer
-          a(href="https://github.com/vdc109/resume-page" target="_blank") Github link
+          
         li
           span
-            b Assistive stick for the blind
+            b Assistive stick for the blind 
+            span (Project link: 
+            a(href="https://github.com/vdc109/Assistive-stick-for-the-blind" target="_blank") https://github.com/vdc109/Assistive-stick-for-the-blind
+            span )
           b.time1 June 2022
           v-spacer
           span Ultrasonic sensor stick warning dangerous obstacles for the blind
           v-spacer
-          a(href="https://github.com/vdc109/Assistive-stick-for-the-blind" target="_blank") Github link
 
 
       h3.head4 EXTRACURRICULARS AND SOCIAL ACTIVITIES
@@ -138,45 +119,45 @@
             b Charity for Children's Education - Ha Giang
           b.time1 June 2022
           v-spacer
-          span Raised 100 million VND, 11 computers, books, bags and clothes from companies and benefactors to donate to poor students in Ha Giang
+          span Raised and donated 100 million VND, 11 computers, books, bags and clothes to poor students in Ha Giang
         li
           a(href="https://drive.google.com/drive/folders/1EoKiGYYm77atDrk78ZFO23vg_2cbWdYc?usp=sharing" target="_blank")
             b Helping the blinds in Nam Dinh
           b.time1 September 2021
           v-spacer
-          span Create supporting devices by 3D printing and Arduino coding for blind people at Nam Dinh Center for the Blind and donate 20 million VND to the people and education funds of blind people
+          span Create supporting devices and donate 20 million VND for blind people at Nam Dinh Center for the Blind
         li
           a(href="https://drive.google.com/drive/folders/11dCBIHj9sc6qXkWzSVgAdscMKKGmBjeA?usp=sharing" target="_blank")
             b Organising events for children in Lao Cai
           b.time1 March 2021
           v-spacer
-          span Create activities and reward students in Lao Cai who excelled in a math competition
+          span Create activities and reward primary school students in Lao Cai who excelled in a math competition
         li
           a(href="https://drive.google.com/drive/folders/1wQZv4ttIhoR1AcL5BdAscElR-NmunkEn?usp=sharing" target="_blank")
             b Raising technology awareness for gifted students in Tuyen Quang
           b.time1 May 2021
           v-spacer
-          span Teaching coding basics in C++, Arduino coding for gift students in Tuyen Quang to create air filtering and monitoring devices
+          span Teaching coding basics and Arduino coding for gifted students in Tuyen Quang to create air filtering and monitoring devices
         li
           span 
-            b Teaching assistant in Math classes for a center
+            b Teaching assistant in Math classes for a local Math center
             b.time1 March 2021 - February 2022
           v-spacer
-          span Assist in teaching, marking homework and communicating with parents of children in online Math classes of an IMO Silver Medalist
+          span Assist in teaching, marking homework and communicating with parents of 7th grade students in online Math classes
         li
           span
             b Making e-commerce website for Tran Le Bookstore
             b.time1 June 2022 - July 2022
           v-spacer
-          span Develop frontend and backend for an English books shopping website
-          v-spacer
-          a(href="https://tranlebookstore.com/" target="_blank") Website link
+          span Develop frontend and backend for an English books shopping website (
+          a(href="https://tranlebookstore.com/" target="_blank") link)
+          
         li
           span
             b Member of GART - Hanoi Amsterdam Robotic Club
             b.time1 September 2020 - Now
           v-spacer
-          span Help teach young students technology and organize STEM related events at schools or the US embassy
+          span Help teach primary school students technology and organize STEM related events at schools or the US embassy
         li
           span
             b Volunteer in STEM summer camp
@@ -185,15 +166,32 @@
           span Help organize STEM summer camp in science experiments for 150 primary school students in Hanoi
         li
           a(href="https://drive.google.com/drive/folders/1MrNQ4lD80jJbxGmMm2NPX8LUxPN93N8M?usp=sharing" target="_blank")
-            b Wrote answer keys for A Closer Look - Ohio grade 1-6
+            b Answer key booklet for A Closer Look - Tennessee grade 1-6
           b.time1 December 2017 - February 2018
           v-spacer
-          span Wrote an answer key book to help financially students who wish to self-study science (from $200 publisher's price to $6)
+          span Wrote an answer key book to help students who wish to self-study science
 
-
-      h3.head2 LEADERSHIP
+      h3.head2 INTERNSHIP AND WORKING EXPERIENCE
       hr.sep-bar
       ul
+        li
+          span
+            b Tran Le Bookstore
+          b.time1 June 2019 - Now
+          v-spacer
+          span Helped create website for the bookstore and manage work related to and review English books (orders, questions, complaints, ...)
+        li
+          span
+            b Hanoi University of Science and Technology Lab
+          b.time1 April 2022 - June 2022
+          v-spacer
+          span Making call bots by creating workflow and connecting to AI call center for companies and industries
+        li
+          span
+            b International MedTech software company ISOFH
+          b.time1 July 2022 - August 2022
+          v-spacer
+          span Fixing bugs and developing the web frontend hosting the company's Blockchain project IVIRSE
         li
           span
             b Co-founder and leader of Edutech club
@@ -201,51 +199,23 @@
           v-spacer Organize events and activities to help unfortunate people in less developed provinces
         li
           span
-            b Admin group Facebook SuTi cùng con hoc Toán
+            b Admin group Facebook SuTi cùng con hoc Toán 
+            span (FB group: 
+            a(href="https://www.facebook.com/groups/3765086493543619" target="_blank") link
+            span )
           b.time1 June 2021 - Now
           v-spacer
           span Sharing online resources, learning tips and methods in STEM and Facebook groups of parents of younger students
           v-spacer
-          a(href="https://www.facebook.com/groups/3765086493543619" target="_blank") Group link
         li
           span
-            b Admin group English Book Corner
+            b Admin group English Book Corner 
+            span (FB group: 
+              a(href="https://www.facebook.com/groups/169378198219139" target="_blank") link
+            span )
           b.time1 April 2021 - Now
           v-spacer
           span Sharing online resources, learning tips and review and recommending books for younger students
-          v-spacer
-          a(href="https://www.facebook.com/groups/169378198219139" target="_blank") Group link
-
-      h3.head2 INTERNSHIP AND WORKING EXPERIENCE
-      hr.sep-bar
-      ul
-        li
-          a(href="https://drive.google.com/drive/folders/1EpOV9QVIRmiI9ZYVI03B5h3capzUx5wZ?usp=sharing" target="_blank")
-            b Hanoi University of Science and Technology Lab
-          b.time1 April 2022 - June 2022
-          v-spacer
-          span Making call bots by creating workflow and connecting to AI call center for companies and industries
-        li
-          a(href="https://drive.google.com/drive/folders/1EqFUZ_aNQj5VX8-mc1d6OJVUzne59V6L?usp=sharing" target="_blank")
-            b International MedTech software company ISOFH
-          b.time1 July 2022 - August 2022
-          v-spacer
-          span Fixing bugs and developing the web frontend hosting the company's Blockchain project IVIRSE
-        li
-          span
-            b Teaching Assistant SuTi Maths
-          b.time1 March 2021 - February 2022
-          v-spacer
-          span Assisted teachers in preparing teaching materials for 2 classes of 25 students and marked students' homework and tests
-        li
-          span
-            b Tran Le Bookstore
-          b.time1 June 2019 - Now
-          v-spacer
-          span Helped create website for the bookstore and manage work related to and review English books (orders, questions, complaints, ...) 
-
-
-      .footer
 </template>
 
 <script lang="ts">
@@ -258,8 +228,9 @@ export default Home;
 
 <style lang="sass">
 .header
+  font-size: 30px
   font-family: Georgia, Serif
-  margin: 20px
+  margin: 10px
 
 .time1
   float: right
@@ -270,14 +241,15 @@ export default Home;
   justify-content: center
 
 .content
-  margin: 15px auto
-  max-width: 85%
+  font-size: 15px
+  margin: 0 auto
+  max-width: 95%
   font-family: Georgia, Serif
 
 .paper
   height: 100%
-  margin: 5px auto
-  max-width: 65%
+  margin: 0px auto
+  max-width: 90%
   background-color: #fff
 
 .root
