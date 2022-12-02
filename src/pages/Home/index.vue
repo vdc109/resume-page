e06522ac293870f9049e4f5e848e23be48a43f5b
vdc109 <93813570+vdc109@users.noreply.github.com>
2022-12-03 02:08:06 +0700

    Update index.vue

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -5,6 +5,10 @@
       v-spacer
       h1.header VINH DO
       v-spacer
+    .contact
+      v-spacer
+      span Email: vinhdc109@gmail.com | Phone: (+84)855595596
+      v-spacer
     .content
       h3.head2 EDUCATION
       hr.sep-bar
@@ -43,7 +47,7 @@
         li
           span Hanoi Informatics Municipal Academic Competition 2021 - 3rd Prize
         li
-          span Mathematical Young Talent Search 2017 - Gold Medal (Competition info: 
+          span Vietnam Mathematical Young Talent Search 2018 - Gold Medal (Competition info: 
           a(href="https://www.hexagon.edu.vn/myts.html" target="_blank") https://www.hexagon.edu.vn/myts.html
           span )
 
@@ -52,42 +56,37 @@
           b International:
         ul
           li
-            span 14th International Mathematics and Science Olympiad 2017 (IMSO) - Silver Medal (Competition info: 
-            a(href="https://www.sofuganda.org/imso" target="_blank") https://www.sofuganda.org/imso
-            span )
+            span Global Link Online Innovation Expo 2022, Japan - 1st Place
           li
             span 7th International Mathematical Modelling Challenge 2021 (IMMC) (Competition info: 
             a(href="https://www.immchallenge.org/Index.html" target="_blank") https://www.immchallenge.org/Index.html
+            span )
+          li
+            span 14th International Mathematics and Science Olympiad 2017 (IMSO) - Silver Medal (Competition info: 
+            a(href="https://www.sofuganda.org/imso" target="_blank") https://www.sofuganda.org/imso
             span )
 
       h3.head3 RESEARCH & PERSONAL PROJECTS
       hr.sep-bar
       ul
         li
-          a(href="https://drive.google.com/drive/folders/1UuoFw0nAvxm-CDYMU7cwCpVYc3ZSqyEQ?usp=sharing" target="_blank")
-            b Global Link Online Innovation Expo 2022, Japan - 1st Place
-          b.time1 August 2022
+          b E-NNOVATE International Innovation Show 2022 - Gold Medal
+          b.time1 Aug 2022
           v-spacer
           span Project: "Fabrication of Vanadium-doped TiO2 for Polluted Wastewater Treatment"
         li
           a(href="https://drive.google.com/drive/folders/1EH29X2PUFuqMjcTEG3_uglgZLeQQ8UIn?usp=sharing" target="_blank")
             b INNOVERSE Innovation & Invention Expo 2022, USA - Gold Medal
-          b.time1 July 2022
+          b.time1 Jul 2022
           v-spacer
           span Project: "Making Use of Speed Braker to Generate Electricity"
-        li 
-          a(href="https://drive.google.com/drive/folders/1FpwChxGljIMtqD07thaTlqGjjj4Bs-M4?usp=sharing" target="_blank")
-            b E-NNOVATE International Innovation Show 2022, Poland - Gold Medal
-          b.time1 June 2022
-          v-spacer
-          span Project: "Design of Distributed Academic-record System based on Blockchain"
         li
           span
             b Resume site   
             span (Project link: 
             a(href="https://github.com/vdc109/resume-page" target="_blank") https://github.com/vdc109/resume-page
             span )
-          b.time1 August 2022 - October 2022
+          b.time1 Aug 2022 - Oct 2022
           v-spacer
           span Resume website based with descriptive links
           v-spacer
@@ -98,112 +97,53 @@
             span (Project link: 
             a(href="https://github.com/vdc109/Assistive-stick-for-the-blind" target="_blank") https://github.com/vdc109/Assistive-stick-for-the-blind
             span )
-          b.time1 June 2022
+          b.time1 Jun 2022
           v-spacer
           span Ultrasonic sensor stick warning dangerous obstacles for the blind
           v-spacer
-
-
-      h3.head4 EXTRACURRICULARS AND SOCIAL ACTIVITIES
-      hr.sep-bar
-      ul
-
-        li
-          a(href="https://drive.google.com/drive/folders/1a7220MDCPqHej8CFgYG1JxOTSmsgcc-O?usp=sharing" target="_blank")
-            b Project Compyter
-          b.time1 May 2022
-          v-spacer
-          span Teaching online Python course for 41 secondary school students and raise 17 million VND for charity
-        li
-          a(href="https://drive.google.com/drive/folders/1hWmt59-pAiEaP9WvgZRrqbFo3GNhCD_N?usp=sharing" target="_blank")
-            b Charity for Children's Education - Ha Giang
-          b.time1 June 2022
-          v-spacer
-          span Raised and donated 100 million VND, 11 computers, books, bags and clothes to poor students in Ha Giang
-        li
-          a(href="https://drive.google.com/drive/folders/1EoKiGYYm77atDrk78ZFO23vg_2cbWdYc?usp=sharing" target="_blank")
-            b Helping the blinds in Nam Dinh
-          b.time1 September 2021
-          v-spacer
-          span Create supporting devices and donate 20 million VND for blind people at Nam Dinh Center for the Blind
-        li
-          a(href="https://drive.google.com/drive/folders/11dCBIHj9sc6qXkWzSVgAdscMKKGmBjeA?usp=sharing" target="_blank")
-            b Organising events for children in Lao Cai
-          b.time1 March 2021
-          v-spacer
-          span Create activities and reward primary school students in Lao Cai who excelled in a math competition
-        li
-          a(href="https://drive.google.com/drive/folders/1wQZv4ttIhoR1AcL5BdAscElR-NmunkEn?usp=sharing" target="_blank")
-            b Raising technology awareness for gifted students in Tuyen Quang
-          b.time1 May 2021
-          v-spacer
-          span Teaching coding basics and Arduino coding for gifted students in Tuyen Quang to create air filtering and monitoring devices
-        li
-          span 
-            b Teaching assistant in Math classes for a local Math center
-            b.time1 March 2021 - February 2022
-          v-spacer
-          span Assist in teaching, marking homework and communicating with parents of 7th grade students in online Math classes
         li
           span
-            b Making e-commerce website for Tran Le Bookstore
-            b.time1 June 2022 - July 2022
+            b E-commerce website for Tran Le Bookstore 
+            span (Link: 
+            a(href="https://tranlebookstore.com/" target="_blank") https://tranlebookstore.com/
+            span )
+          b.time1 2021 - 2022
           v-spacer
-          span Develop frontend and backend for an English books shopping website (
-          a(href="https://tranlebookstore.com/" target="_blank") link)
-          
-        li
-          span
-            b Member of GART - Hanoi Amsterdam Robotic Club
-            b.time1 September 2020 - Now
+          span Contribute to development of frontend and backend for an English books shopping website
           v-spacer
-          span Help teach primary school students technology and organize STEM related events at schools or the US embassy
-        li
-          span
-            b Volunteer in STEM summer camp
-            b.time1 August 2018
-          v-spacer
-          span Help organize STEM summer camp in science experiments for 150 primary school students in Hanoi
-        li
-          a(href="https://drive.google.com/drive/folders/1MrNQ4lD80jJbxGmMm2NPX8LUxPN93N8M?usp=sharing" target="_blank")
-            b Answer key booklet for A Closer Look - Tennessee grade 1-6
-          b.time1 December 2017 - February 2018
-          v-spacer
-          span Wrote an answer key book to help students who wish to self-study science
-
+      
       h3.head2 INTERNSHIP AND WORKING EXPERIENCE
       hr.sep-bar
       ul
         li
           span
             b Tran Le Bookstore
-          b.time1 June 2019 - Now
+          b.time1 Jun 2019 - Now
           v-spacer
-          span Helped create website for the bookstore and manage work related to and review English books (orders, questions, complaints, ...)
+          span Content writer for the bookstore FB page with 11000 members and manage work related to marketing and reviewing English books 
         li
           span
             b Hanoi University of Science and Technology Lab
-          b.time1 April 2022 - June 2022
+          b.time1 Apr 2022 - Jun 2022
           v-spacer
-          span Making call bots by creating workflow and connecting to AI call center for companies and industries
+          span Developing call bots by creating workflow and connecting to AI call center for companies and industries
         li
           span
             b International MedTech software company ISOFH
-          b.time1 July 2022 - August 2022
+          b.time1 Jul 2022 - Aug 2022
           v-spacer
           span Fixing bugs and developing the web frontend hosting the company's Blockchain project IVIRSE
-        li
-          span
-            b Co-founder and leader of Edutech club
-            b.time1 November 2021 - Now
-          v-spacer Organize events and activities to help unfortunate people in less developed provinces
+      
+      h3.head2 LEADERSHIP
+      hr.sep-bar
+      ul
         li
           span
             b Admin group Facebook SuTi cùng con hoc Toán 
             span (FB group: 
             a(href="https://www.facebook.com/groups/3765086493543619" target="_blank") link
             span )
-          b.time1 June 2021 - Now
+          b.time1 Jun 2021 - Now
           v-spacer
           span Sharing online resources, learning tips and methods in STEM and Facebook groups of parents of students 6 to 13
           v-spacer
@@ -213,9 +153,55 @@
             span (FB group: 
               a(href="https://www.facebook.com/groups/169378198219139" target="_blank") link
             span )
-          b.time1 April 2021 - Now
+          b.time1 Apr 2021 - Now
           v-spacer
           span Sharing online resources, learning tips and review and recommending books for students 6 to 13
+        li
+          span
+            b Co-founder and leader of Edutech club
+            b.time1 Nov 2021 - Now
+          v-spacer Organize STEM classes, events and activities to raise funds to help unfortunate people in less developed provinces
+          
+      h3.head4 EXTRACURRICULARS AND SOCIAL ACTIVITIES
+      hr.sep-bar
+      ul
+        li
+          b Teaching and teaching assistant in STEM subjects for centers and projects
+          b.time1 2020 - Now
+          ul
+            li
+              span Teaching online Python course for 41 secondary school students to raise funds in 
+              a(href="https://drive.google.com/drive/folders/1a7220MDCPqHej8CFgYG1JxOTSmsgcc-O?usp=sharing" target="_blank") Project Compyter.
+            li
+              span Teach Arduino coding for gifted students to make air monitoring device in 
+              a(href="https://drive.google.com/drive/folders/1wQZv4ttIhoR1AcL5BdAscElR-NmunkEn?usp=sharing" target="_blank") Tuyen Quang.
+        li
+          b Volunteering and charity activities
+          b.time1 2021 - Now
+          ul
+            li
+              span Raised and donated 100 million VND, 11 computers, books, bags and clothes to poor students in 
+              a(href="https://drive.google.com/drive/folders/1hWmt59-pAiEaP9WvgZRrqbFo3GNhCD_N?usp=sharing" target="_blank") Ha Giang.
+            li
+              span Create supporting devices and donate 20 million VND for blind people at the Blind Center in     
+              a(href="https://drive.google.com/drive/folders/1EoKiGYYm77atDrk78ZFO23vg_2cbWdYc?usp=sharing" target="_blank") Nam Dinh.
+          v-spacer
+          
+        li
+          b Club: 
+          b.time1 2020 - Now
+          span
+            b GART 6520 - Hanoi Amsterdam Robotic Club & HFC - Hanoi Amsterdam Football Club. 
+          v-spacer
+          span Help teach primary school students technology and organize STEM related events at schools or the US embassy. Help organize school soccer cup and participating in the cup itself
+        li
+          b Publication:
+          b.time1 2017 - 2018
+          a(href="https://drive.google.com/drive/folders/1MrNQ4lD80jJbxGmMm2NPX8LUxPN93N8M?usp=sharing" target="_blank")
+            b Answer key booklet for A Closer Look - Tennessee grade 1-6
+          v-spacer
+          span Wrote an answer key book to help students who wish to self-study science
+        
 </template>
 
 <script lang="ts">
@@ -236,10 +222,15 @@ export default Home;
   float: right
 
 .title
-  margin: 5px auto
+  margin: 5px auto 0 auto
   display: flex
   justify-content: center
 
+.contact
+  display: flex
+  font-family: Georgia, Serif
+  justify-content: center
+  
 .content
   font-size: 15px
   margin: 0 auto
