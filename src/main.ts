c9778831d5129ddac5946e405daefde8ca8ccb71
vdc109 <93813570+vdc109@users.noreply.github.com>
2023-10-04 13:06:04 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuetify from 'vuetify'
-import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
